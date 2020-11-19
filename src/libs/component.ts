38faac5ac212e7834a9d689d2d511779dc065cd3
uihyeon.cha <uihyeon.cha@gmail.com>
2020-11-20 00:46:42 +0900

    컴포넌트 추상화 클래스 개선

--- src/libs/component.ts
+++ src/libs/component.ts
@@ -8,8 +8,12 @@ export abstract class Component<Props = KeyValue> {
     static defaultProps = {}
 
     #uid: string
-    #el!: HTMLElement | null
+    #target: HTMLElement | null = null
+    #targetHTML = ''
+    #beforeUnmountCallback = () => {}
+    #el: HTMLElement | null = null
     #props = {} as Props
+    #mounted = false
 
     constructor(props?: Props) {
         this.#uid = genUid()
@@ -31,13 +35,54 @@ export abstract class Component<Props = KeyValue> {
         return this.#props
     }
 
+    /**
+     * Render
+     *
+     * - 템플릿 루트는 반드시 하나의 요소로 이루어져 있어야함.
+     * - 템플릿 루트는 반드시 `id=${this.uid}` 속성을 포함해야함.
+     *   - `<div id=${this.uid}>...</div>`
+     */
     public abstract render(): string
 
     public mount(target: HTMLElement | null) {
         if (!target) {
             throw new Error('target이 없습니다.')
         }
-        target.innerHTML = this.render()
+        this.#target = target
+        this.#targetHTML = target.innerHTML
+        this.#target.innerHTML = this.render()
+    }
+
+    public unmount(): void {
+        this.#mounted = false
+        this.#beforeUnmountCallback?.()
+        this.#beforeUnmountCallback = () => {}
+        if (this.#target) {
+            this.#target.innerHTML = this.#targetHTML
+        }
+    }
+
+    /**
+     * On mounted
+     *
+     * - 첫번째 렌더링에서만 실행됨.
+     * - render 메서드 안에서 호출해야함.
+     */
+    public onMounted(cb: () => void) {
+        if (!this.#mounted) {
+            this.#mounted = true
+            cb?.()
+        }
+    }
+
+    /**
+     * On before unmount
+     *
+     * unmount 메서드를 사용했을 때 실행됨.
+     * render 메서드 안에서 호출해야함.
+     */
+    public onBeforeUnmount(cb: () => void) {
+        this.#beforeUnmountCallback = cb
     }
 }
 
