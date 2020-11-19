94d5c274d60dd3e8181f9c7c7a27610ed0c82444
uihyeon.cha <uihyeon.cha@gmail.com>
2020-11-20 00:56:57 +0900

    텍스트 컴포넌트 추가

---
+++ src/components/ui/Text.ts
@@ -0,0 +1,34 @@
+import { Component } from '~/libs/component'
+
+export interface Props {
+    value: string | number
+}
+
+export class Text extends Component<Props> {
+    static defaultProps = {
+        value: '',
+    }
+
+    #value = ''
+
+    get value() {
+        return this.#value
+    }
+
+    render() {
+        this.setValue(this.props.value)
+
+        return `<i id=${this.uid}>${this.value}</i>`
+    }
+
+    public setValue(nextValue: string | number) {
+        const value = String(nextValue)
+        this.#value = value
+
+        if (this.el) {
+            this.el.innerText = this.#value
+        }
+    }
+}
+
+export default Text
