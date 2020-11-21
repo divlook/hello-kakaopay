20f2ef8e27c8debe12d3a5d745e3ff9d35b36431
uihyeon.cha <uihyeon.cha@gmail.com>
2020-11-22 05:55:21 +0900

    uid.ts 유닛 테스트 추가

---
+++ tests/libs/uid.test.ts
@@ -0,0 +1,12 @@
+import { genUid } from '~/libs/uid'
+
+describe('getUid', () => {
+    test(`첫번째 uid는 "uid-1"입니다.`, () => {
+        const uid = genUid()
+        expect(uid).toBe('uid-1')
+    })
+    test(`두번째 uid는 "uid-2"입니다.`, () => {
+        const uid = genUid()
+        expect(uid).toBe('uid-2')
+    })
+})
